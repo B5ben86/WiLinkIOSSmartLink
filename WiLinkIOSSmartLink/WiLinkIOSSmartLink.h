45289ee60f0bf5b08cfbe309af3a0eca41747367
Ben Huang <b5ben86@163.com>
2022-07-29 14:43:09 +0800

    增加 public header

--- WiLinkIOSSmartLink/WiLinkIOSSmartLink.h
+++ WiLinkIOSSmartLink/WiLinkIOSSmartLink.h
@@ -15,4 +15,7 @@ FOUNDATION_EXPORT const unsigned char WiLinkIOSSmartLinkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <WiLinkIOSSmartLink/PublicHeader.h>
 
-#import <SmartLinkSDK.h>
+//TODO: 若发布新版本后 pod search 无法找到新版本，则运行以下命令后重新尝试
+// rm ~/Library/Caches/CocoaPods/search_index.json
+
+#import <WiLinkIOSSmartLink/SmartLinkSDK.h>
