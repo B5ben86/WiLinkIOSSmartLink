210ce9f526e546533944fe5619f226983568ce57
Ben Huang <b5ben86@163.com>
2022-08-18 23:55:26 +0800

    删除无用头文件

--- WiLinkIOSSmartLink/WiLinkIOSSmartLink.h
+++ WiLinkIOSSmartLink/WiLinkIOSSmartLink.h
@@ -19,5 +19,5 @@ FOUNDATION_EXPORT const unsigned char WiLinkIOSSmartLinkVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <WiLinkIOSSmartLink/PublicHeader.h>
 
 #import <WiLinkIOSSmartLink/SmartLinkSDK.h>
-#import <WiLinkIOSSmartLink/GCDAsyncSocket.h>
-#import <WiLinkIOSSmartLink/GCDAsyncUdpSocket.h>
+//#import <WiLinkIOSSmartLink/GCDAsyncSocket.h>
+//#import <WiLinkIOSSmartLink/GCDAsyncUdpSocket.h>
